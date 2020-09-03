de5d8f0156cc96c923355198fcab385ba278b5dd
Elena Bialyk <ebialyk@ruckuswireless.com>
2020-09-03 14:54:07 +0300

    first commit

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,59 @@
+import { Todo } from './todo.class';
+
+export class TodoList {
+
+    constructor() {
+
+        this.loadLocalStorage();
+
+    }
+
+    newTodo(todo) {
+        this.todos.push(todo);
+        this.saveToLocalStorage();
+    }
+
+    deleteAll(id) {
+
+        this.todos = this.todos.filter(todo => todo.id != id)
+        this.saveToLocalStorage();
+    }
+
+    toggleCompleted(id) {
+
+        for (const todo of this.todos) {
+
+            if (todo.id == id) {
+
+                todo.completado = !todo.completado;
+                this.saveToLocalStorage();
+                break;
+            }
+
+        }
+
+
+    }
+
+    removeCompleted() {
+
+        this.todos = this.todos.filter(todo => !todo.completed)
+        this.saveToLocalStorage();
+    }
+
+    saveToLocalStorage() {
+
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+
+    }
+
+    loadLocalStorage() {
+
+        this.todos = (localStorage.getItem('todo')) ?
+            JSON.parse(localStorage.getItem('todo')) :
+            [];
+
+        this.todos = this.todos.map(Todo.fromJson);
+    }
+
+}
---
+++ src/classes/todo.class.js
@@ -0,0 +1,28 @@
+export class Todo {
+
+    static fromJson({ id, task, completed, created }) {
+
+        const tempTodo = new Todo(task);
+
+        tempTodo.id = id;
+        tempTodo.completed = completed;
+        tempTodo.created = created;
+
+        return tempTodo;
+    }
+
+    constructor(task) {
+
+        this.task = task;
+
+        this.id = new Date().getTime(); // 12836871263
+        this.completed = false;
+        this.created = new Date();
+
+    }
+
+    printClass() {
+        console.log(`${ this.task } - ${ this.id }`);
+    }
+
+}
